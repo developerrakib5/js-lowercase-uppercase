7a8cd8c9c7276c402e4c1efbb3432eeb75e76cc7
devlrakib <devrakib5@gmail.com>
2024-03-21 10:47:05 +0600

    first commit

---
+++ js-string-problem.js
@@ -0,0 +1,31 @@
+// Case Javascript Code
+let userName = 'blackPink';
+let userInput = 'blackpinK';
+// toLowerCase
+if (userName.toLowerCase() === userInput.toLowerCase()){
+    console.log('Valid User toLowerCase');
+}
+else{
+    console.log('Invalid User');
+}
+// toUpperCase
+if (userName.toUpperCase() === userInput.toUpperCase()){
+    console.log('Valid User toUpperCase');
+}
+else{
+    console.log('Invalid User');
+}
+// toLocaleLowerCase
+if (userName.toLocaleLowerCase() === userInput.toLocaleLowerCase()){
+    console.log('Valid User LocaleLowerCase');
+}
+else{
+    console.log('Invalid User');
+}
+// toUpperCase
+if (userName.toLocaleUpperCase() === userInput.toLocaleUpperCase()){
+    console.log('Valid User toLocaleUpperCase');
+}
+else{
+    console.log('Invalid User');
+}
